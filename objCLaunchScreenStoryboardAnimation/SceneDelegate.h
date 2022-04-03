88fae4a58d0d1d34d8c1d25c3068acc660d717a5
saiashish9 <saiashish3760@gmail.com>
2022-04-04 00:08:03 +0530

    completed

--- objCLaunchScreenStoryboardAnimation/SceneDelegate.h
+++ objCLaunchScreenStoryboardAnimation/SceneDelegate.h
@@ -1,10 +1,3 @@
-//
-//  SceneDelegate.h
-//  objCLaunchScreenStoryboardAnimation
-//
-//  Created by Sai Ashish Darapureddy on 28/03/22.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
